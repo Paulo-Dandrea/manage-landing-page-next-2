e655069721db3ef697474f56b7f653a5856bef8c
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-05-26 11:38:06 -0300

    feat: make scss font variable, using NEXT importing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import s from "./styles/page.module.scss";
+import { Be_Vietnam_Pro } from "next/font/google";
+
+// export const be_vietnam_pro = Be_Vietnam_Pro({
+//   variable: "--vietnam-pro",
+//   weight: ['400', '500', '700'],
+//   subsets: ["latin"],
+// });
 
 export default function Home() {
   return (
-    <main className={s.main} >
-      <h1>hello</h1>
+    <main className={s.main}>
+      <h1>Whereas recognition of the inherent dignity</h1>
     </main>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <main className={be_vietnam_pro.className} >
+//       <h1>Whereas recognition of the inherent dignity</h1>
+//     </main>
+//   );
+// }
